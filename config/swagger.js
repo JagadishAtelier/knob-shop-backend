63bb499a8dd4e50d36099d4dfdd0a47c696b594a
prasanthoyasco <prasanthoyasco@gmai.com>
2025-07-25 12:07:05 +0530

    swagger server updated

--- config/swagger.js
+++ config/swagger.js
@@ -14,7 +14,7 @@ const swaggerOptions = {
         url: "http://localhost:5000/api", // change as needed
       },
       {
-        url: "https://knob-shop-backend.onrender.com/api-docs/api", // change as needed
+        url: "https://knob-shop-backend.onrender.com/api", // change as needed
       },
     ],
     components: {
