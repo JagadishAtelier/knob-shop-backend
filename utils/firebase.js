d5000038d47458accb160532fc808b29d7b58334
JagadishRamakrishnan <jeevasumathi1357@gmail.com>
2025-08-01 11:53:07 +0530

    updated authentication api

---
+++ utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCj3aUUlCltGEJs8mDPxKP-ueSNGnz8Brg",
+  authDomain: "knob-shop-89776.firebaseapp.com",
+  projectId: "knob-shop-89776",
+  storageBucket: "knob-shop-89776.firebasestorage.app",
+  messagingSenderId: "728511396495",
+  appId: "1:728511396495:web:b08c0b56fd522c4d214efa",
+  measurementId: "G-EW3KSD5B54"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
