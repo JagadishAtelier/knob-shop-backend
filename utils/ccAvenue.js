e7804fd0af7289eca2b15d8c9ee8e634f65a42bf
prasanthoyasco <prasanthoyasco@gmai.com>
2025-08-07 21:05:25 +0530

    added payment inititate api text

--- utils/ccAvenue.js
+++ utils/ccAvenue.js
@@ -2,13 +2,13 @@ const CryptoJS = require("crypto-js");
 
 function encrypt(text, workingKey) {
   const key = CryptoJS.enc.Utf8.parse(workingKey);
-  const iv = CryptoJS.enc.Utf8.parse(workingKey);
+  const iv = CryptoJS.enc.Utf8.parse(workingKey); // Ensure 16 characters
   const encrypted = CryptoJS.AES.encrypt(text, key, {
     iv,
     mode: CryptoJS.mode.CBC,
     padding: CryptoJS.pad.Pkcs7,
   });
-  return encrypted.ciphertext.toString(CryptoJS.enc.Hex); // 🔁 Use .ciphertext + Hex
+  return encrypted.toString(); // ✅ This returns full base64
 }
 
 module.exports = encrypt;
