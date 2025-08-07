f000f1fbc7dfaa92c75198dc3c08a563fb946892
prasanthoyasco <prasanthoyasco@gmai.com>
2025-08-07 21:31:50 +0530

    payment api added

--- utils/ccAvenue.js
+++ utils/ccAvenue.js
@@ -2,13 +2,13 @@ const CryptoJS = require("crypto-js");
 
 function encrypt(text, workingKey) {
   const key = CryptoJS.enc.Utf8.parse(workingKey);
-  const iv = CryptoJS.enc.Utf8.parse(workingKey); // Ensure 16 characters
+  const iv = CryptoJS.enc.Utf8.parse(workingKey);
   const encrypted = CryptoJS.AES.encrypt(text, key, {
     iv,
     mode: CryptoJS.mode.CBC,
     padding: CryptoJS.pad.Pkcs7,
   });
-  return encrypted.toString(); // ✅ This returns full base64
+  return encrypted.toString();
 }
 
 module.exports = encrypt;
