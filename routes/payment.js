c0530259bd81e89748de80d3658f6bedf2258914
prasanthoyasco <prasanthoyasco@gmai.com>
2025-08-07 21:31:50 +0530

    payment api added

--- routes/payment.js
+++ routes/payment.js
@@ -18,7 +18,8 @@ router.post("/initiate", async (req, res) => {
     billing_country,
   } = req.body;
 
-  const formData = `merchant_id=${merchantId}&order_id=${orderId}&currency=${currency}&amount=${amount}&redirect_url=${redirectUrl}&cancel_url=${cancelUrl}&language=EN&billing_name=${billing_name}&billing_address=${billing_address}&billing_city=${billing_city}&billing_state=${billing_state}&billing_zip=${billing_zip}&billing_country=${billing_country}&billing_tel=${billing_tel}&billing_email=${billing_email}`;
+  const formData = `merchant_id=${encodeURIComponent(merchantId)}&order_id=${encodeURIComponent(orderId)}&currency=${encodeURIComponent(currency)}&amount=${encodeURIComponent(amount)}&redirect_url=${encodeURIComponent(redirectUrl)}&cancel_url=${encodeURIComponent(cancelUrl)}&language=EN&billing_name=${encodeURIComponent(billing_name)}&billing_address=${encodeURIComponent(billing_address)}&billing_city=${encodeURIComponent(billing_city)}&billing_state=${encodeURIComponent(billing_state)}&billing_zip=${encodeURIComponent(billing_zip)}&billing_country=${encodeURIComponent(billing_country)}&billing_tel=${encodeURIComponent(billing_tel)}&billing_email=${encodeURIComponent(billing_email)}`;
+
 console.log("Form Data:", formData);
   const encRequest = encrypt(formData, workingKey);
 console.log("encRequest", encRequest);
