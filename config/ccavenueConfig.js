81b6d19056dff83530a2cf685f3dcea0bd8d09ae
prasanthoyasco <prasanthoyasco@gmai.com>
2025-08-08 12:08:55 +0530

    payment api modified

--- config/ccavenueConfig.js
+++ config/ccavenueConfig.js
@@ -2,6 +2,6 @@ module.exports = {
   merchantId: "4397359",
   accessCode: "AVCV82MH89AJ27VCJA",
   workingKey: "34B1D040BCA38353928C3FE8AA48C69C", // for encryption
-  redirectUrl: "https://knobsshop.store/payment-response",
-  cancelUrl: "https://knobsshop.store/payment-cancelled",
+  redirectUrl: "https://knobsshop.store/order-confirmed",
+  cancelUrl: "https://knobsshop.store/payment-failed",
 };
