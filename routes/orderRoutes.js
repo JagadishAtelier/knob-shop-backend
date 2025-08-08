9f8c1548278656484d19892017ce7229a23e3a68
prasanthoyasco <prasanthoyasco@gmai.com>
2025-08-08 18:21:16 +0530

    get ordersbu user id api add

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,11 +1,12 @@
 
 const express = require("express");
 const router = express.Router();
-const { createOrderWithShipping,getAllOrders,getOrderById,deleteOrderById,updateOrderByOrderId } = require("../controllers/orderController");
+const { createOrderWithShipping,getAllOrders,getOrderById,deleteOrderById,updateOrderByOrderId,getOrdersByUserId } = require("../controllers/orderController");
 
 router.post("/", createOrderWithShipping);
 router.get("/", getAllOrders);
 router.get("/:id", getOrderById);
 router.put("/:orderId", updateOrderByOrderId);
+router.get("/user/:userId", getOrdersByUserId);
 router.delete("/:id", deleteOrderById);
 module.exports = router;
