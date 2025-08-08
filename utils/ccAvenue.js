d532f27e00900f6eed4256ae373a52f238b09853
prasanthoyasco <prasanthoyasco@gmai.com>
2025-08-08 11:21:51 +0530

    payment api merchentid respose sent

--- utils/ccAvenue.js
+++ utils/ccAvenue.js
@@ -1,14 +1,24 @@
-const CryptoJS = require("crypto-js");
+var crypto = require('crypto');
+exports.encrypt = function (plainText, workingKey) {
+	var m = crypto.createHash('md5');
+    	m.update(workingKey);
+   	var key = m.digest('binary');
+      	var iv = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f';	
+	var cipher = crypto.createCipheriv('aes-128-cbc', key, iv);
+	var encoded = cipher.update(plainText,'utf8','hex');
+	encoded += cipher.final('hex');
+    	return encoded;
+};
 
-function encrypt(text, workingKey) {
-  const key = CryptoJS.enc.Utf8.parse(workingKey);
-  const iv = CryptoJS.enc.Utf8.parse(workingKey);
-  const encrypted = CryptoJS.AES.encrypt(text, key, {
-    iv,
-    mode: CryptoJS.mode.CBC,
-    padding: CryptoJS.pad.Pkcs7,
-  });
-  return encrypted.toString();
-}
 
-module.exports = encrypt;
+exports.decrypt = function (encText, workingKey) {
+    	var m = crypto.createHash('md5');
+    	m.update(workingKey)
+    	var key = m.digest('binary');
+	var iv = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f';	
+	var decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
+    	var decoded = decipher.update(encText,'hex','utf8');
+	decoded += decipher.final('utf8');
+    	return decoded;
+};
+
