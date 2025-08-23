7a940b378deafc2dc3d84124b3ea087ee855c4ea
JagadishRamakrishnan <jeevasumathi1357@gmail.com>
2025-08-23 18:10:02 +0530

    updated admin updation for profile image

--- models/User.js
+++ models/User.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true, trim: true, lowercase: true },
   password: { type: String, required: true },
   phone: { type: String, unique: true, sparse: true },
+  profileUrl: { type: String, default: "" },
   role: {
     type: String,
     enum: ['user', 'admin'],
