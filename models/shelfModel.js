b369d1bd52bb0a4190d6e8418bed1661d0d8fbf8
JagadishRamakrishnan <jeevasumathi1357@gmail.com>
2025-08-25 16:08:16 +0530

    updated shelf api

---
+++ models/shelfModel.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const shelfSchema = new mongoose.Schema({
+    heading: { type: String, required: true },
+    content: { type: String, required: true },
+    buttonText: { type: String, required: true },
+    imageUrl: { type: String, default: "" }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Shelf", shelfSchema);
