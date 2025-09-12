37086b0977015cb0ef2994fd71ffc4eb3c7c638f
prasanthoyasco <prasanthoyasco@gmai.com>
2025-09-12 10:13:49 +0530

    company and gst field added to frontuser

--- models/FrontUser.js
+++ models/FrontUser.js
@@ -2,27 +2,41 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 // Sub-schema: Payment Record
-const paymentSchema = new mongoose.Schema({
-  amount: Number,
-  date: { type: Date, default: Date.now },
-  method: String,
-  status: String,
-  orderId: String,
-}, { _id: false });
+const paymentSchema = new mongoose.Schema(
+  {
+    amount: Number,
+    date: { type: Date, default: Date.now },
+    method: String,
+    status: String,
+    orderId: String,
+  },
+  { _id: false }
+);
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, default: "User" },
-  email: { type: String, unique: true, sparse: true, trim: true, lowercase: true },
-  phone: { type: String, unique: true, sparse: true },
-  profileUrl: { type: String, default: "" },
-  gender: { type: String},
-  password: { type: String, required: true },
-  usedCoupons: [{ type: String }],
-  dateofbirth: { type: Date },
-  address: [{ type: mongoose.Schema.Types.ObjectId, ref: "Address" }],
-  wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
-  paymentHistory: [paymentSchema],
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, default: "User" },
+    email: {
+      type: String,
+      unique: true,
+      sparse: true,
+      trim: true,
+      lowercase: true,
+    },
+    phone: { type: String, unique: true, sparse: true },
+    profileUrl: { type: String, default: "" },
+    gender: { type: String },
+    company: { type: String, default: "" },
+    GST: { type: String, default: "" },
+    password: { type: String, required: true },
+    usedCoupons: [{ type: String }],
+    dateofbirth: { type: Date },
+    address: [{ type: mongoose.Schema.Types.ObjectId, ref: "Address" }],
+    wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+    paymentHistory: [paymentSchema],
+  },
+  { timestamps: true }
+);
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
