2a37df9c24fc74ce5cc69352bc1345d837e3a7e4
prasanthoyasco <prasanthoyasco@gmai.com>
2025-12-10 10:53:47 +0530

    image size expanded

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ connectDB();
 
 const app = express();
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "300mb" }));
+app.use(express.urlencoded({ extended: true, limit: "300mb" }));
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
