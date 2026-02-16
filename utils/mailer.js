ef67569259615f871da23f8745d9d62a5ce628ec
prasanthoyasco <prasanthoyasco@gmai.com>
2026-02-16 12:22:05 +0530

    Allow MAIL_PORT from env in mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,10 +1,12 @@
 const nodemailer = require("nodemailer");
 require('dotenv').config();
 
+const port = parseInt(process.env.MAIL_PORT) || 587;
+
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: 465, // Use SSL
-  secure: true, // true for 465, false for other ports
+  port: port,
+  secure: port === 465, // true for 465, false for other ports
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
